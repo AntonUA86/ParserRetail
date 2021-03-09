3839a70652d794341e068dcf2de8390f8c79ff9b
Антон <antonk095895@gmail.com>
2021-03-09 16:17:07 +0200

    Модель-представления главного окна команду выхода из приложения

--- View/ViewModels/MainWindowsViewModel.cs
+++ View/ViewModels/MainWindowsViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using View.Infrastructure.Commands;
 using View.ViewModels.Base;
 
 
@@ -8,8 +11,29 @@ namespace View.ViewModels
 {
     internal class MainWindowsViewModel : ViewModel
     {
+        #region Заголовок окна
         private string _Title = "Анализ цен";
 
         public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+
+        #region Команды
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p) 
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+        #endregion
+
+        public MainWindowsViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            #endregion
+        }
     }
 }
