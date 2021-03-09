4099967a2784087e8f9c7fb8f577ed5faba3173c
Антон <antonk095895@gmail.com>
2021-03-09 13:39:48 +0200

    Изменина структура XAML. Добавил DataContext.

---
+++ View/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using View.ViewModels.Base;
+
+
+namespace View.ViewModels
+{
+    internal class MainWindowsViewModel : ViewModel
+    {
+        private string _Title = "Анализ цен";
+
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+    }
+}
