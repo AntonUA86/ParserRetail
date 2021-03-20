22281071bac3305df6735da288a2a0e6c9818413
Антон <antonk095895@gmail.com>
2021-03-20 12:46:50 +0200

    Тест График

---
+++ Parser/Models/DataPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser.Models
+{
+  public  struct DataPoint
+    {
+        public double XValue { get; set; }
+
+        public double YValue { get; set; }
+    }
+}
